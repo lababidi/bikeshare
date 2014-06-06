d24b7031a2db75c9bb54b510dcc200f5193b5adb
lababidi <mlababidi@macewindu.home>
2014-06-06 07:40:44 -0400

    Initial Commit

---
+++ parsestations.py
@@ -0,0 +1,36 @@
+import xmltodict
+import csv
+import requests
+doc = xmltodict.parse(open("bikeStations.xml").read())
+stations = {}
+bikes = []
+journeys = {}
+print doc
+print len(doc['stations']['station'][0])
+print doc['stations']['station'][0].keys()
+for station in  doc['stations']['station']:
+	print station
+	stations.update({station['terminalName']:(station['lat'],station['long'])})
+print stations
+with open('2013-3rd-quarter.csv') as csvfile:
+	bikesreader = csv.DictReader(csvfile)
+	for bike in  bikesreader:
+		print bike
+		if bike['Start terminal'] in stations and bike['End terminal'] in stations:
+			bike.update({'startgeo':stations[bike['Start terminal']]})
+			bike.update({'endgeo':stations[bike['End terminal']]})
+			bike.update({'startend':bike['Start terminal']+'_'+bike['End terminal']})
+			bikes.append( bike)
+			journeys.update({bike['startend']:(bike['startgeo'],bike['endgeo'])})
+	print bikes
+	print len(bikes)
+	for key,j in journeys.iteritems():
+		start = ','.join(j[0])
+		end = ','.join(j[1])
+		print start,end
+		url = "https://maps.googleapis.com/maps/api/directions/json?origin="+start+"&destination="+end+"&sensor=false&key=AIzaSyAMCocd925ayO9xxf4wwT-XkL94Gf8GuzY&avoid=highways&mode=bicycling"
+		r = requests.get(url)
+		w = open("googledirections/"+key,'w')
+		w.write(r.text.encode('utf-8'))
+		w.close()
+	print len(journeys)
